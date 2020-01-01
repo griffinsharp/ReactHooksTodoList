659eaa4e045e5ae3fc37eb16cf8b4d185ff51117
griffin sharp <gsharpdev@gmail.com>
2019-12-31 19:11:07 -0800

    Add todo list delete button and add list component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ToDoForm from './TodoForm';
+import TodoList from "./TodoList";
 import Typography from '@material-ui/core/Typography';
 import './App.css';
 
 function App() {
+
+  // initial state of our todo list is an empty array
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
@@ -11,6 +16,7 @@ function App() {
       </Typography>
 
       <ToDoForm saveTodo={console.warn}/>
+      <TodoList todos={todos} />
     </div>
   );
 }
